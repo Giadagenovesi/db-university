1602e004f1de4821c5b6db0561007b2c0d7da645
Giadagenovesi <giada.g@outlook.it>
2023-05-12 15:32:54 +0200

    Completed QUERY 4,5,6

--- university_db.sql
+++ university_db.sql
@@ -5,7 +5,7 @@ WHERE `date_of_birth`
 LIKE "1990%";
 
 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
-SELECT `name` `cfu` 
+SELECT `name`,`cfu` 
 FROM `courses`
 WHERE `cfu` > 10;
 
@@ -13,11 +13,23 @@ WHERE `cfu` > 10;
 
 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
 laurea (286)
+SELECT `name`, `period`,`year`
+FROM `courses` 
+WHERE `year` = 1 
+AND `period` = 'I semestre';
 
 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
-20/06/2020 (21)
+20/06/2020' (21)
+SELECT *
+FROM `exams` 
+WHERE `hour` > "14%" 
+AND `date` = '2020-06-20';
+
 
 6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT `name`, `level` 
+FROM `degrees`
+WHERE `level` = "magistrale";
 
 7. Da quanti dipartimenti è composta l'università? (12)
 
