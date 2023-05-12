2abfe34fbedf5df8c01c46ecdc5ed2c8a73755bb
Giadagenovesi <giada.g@outlook.it>
2023-05-12 15:42:32 +0200

    Completed QUERY 7, 8

--- university_db.sql
+++ university_db.sql
@@ -1,10 +1,12 @@
 1. Selezionare tutti gli studenti nati nel 1990 (160)
+
 SELECT *
 FROM `students`
 WHERE `date_of_birth`
 LIKE "1990%";
 
 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
+
 SELECT `name`,`cfu` 
 FROM `courses`
 WHERE `cfu` > 10;
@@ -13,6 +15,7 @@ WHERE `cfu` > 10;
 
 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
 laurea (286)
+
 SELECT `name`, `period`,`year`
 FROM `courses` 
 WHERE `year` = 1 
@@ -20,6 +23,7 @@ AND `period` = 'I semestre';
 
 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
 20/06/2020' (21)
+
 SELECT *
 FROM `exams` 
 WHERE `hour` > "14%" 
@@ -27,10 +31,18 @@ AND `date` = '2020-06-20';
 
 
 6. Selezionare tutti i corsi di laurea magistrale (38)
+
 SELECT `name`, `level` 
 FROM `degrees`
 WHERE `level` = "magistrale";
 
-7. Da quanti dipartimenti è composta l'università? (12)
+7. Da quanti dipartimenti è composta l'università? (12)'
+
+SELECT COUNT(*) AS `departments_number`
+FROM `departments`;
 
 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+
+SELECT COUNT(*) AS `whitout_phone` 
+FROM `teachers`
+WHERE `phone` IS NULL;
