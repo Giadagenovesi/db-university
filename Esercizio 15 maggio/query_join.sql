1e0dfb2d4a8fa66a7240a55ee788512da37b59f8
Giadagenovesi <giada.g@outlook.it>
2023-05-15 17:40:53 +0200

    completed QUERY 6

--- Esercizio 15 maggio/query_join.sql
+++ Esercizio 15 maggio/query_join.sql
@@ -49,5 +49,18 @@ ON `course_teacher`.`teacher_id` = `teachers`.`id`;
 
 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
+SELECT `teachers`.`id` AS `teacher_id`, `teachers`.`surname` AS `teacher_name`, `departments`.`name` AS `department_name`
+FROM `departments` 
+JOIN `degrees`
+ON `departments`.`id` = `degrees`.`department_id`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `course_teacher`.`teacher_id` = `teachers`.`id`
+WHERE `departments`.`name` = 'Dipartimento di Matematica'
+ORDER BY `teachers`.`id` ASC;
+
 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
 superare ciascuno dei suoi esami
